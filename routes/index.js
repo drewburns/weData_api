0b9a6e95b4f6a43ed01830c338cd24c16d3eb4e1
drewburns <drewburnsbab@gmail.com>
2020-08-28 23:14:21 -0400

    fake data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/fakeData1", function (req, res, next) {
+  const fakeData = [
+    {
+      shipmentID: 1,
+      arrivalDate: "June 3,2020 4:40PM EST",
+      port: "Los Angeles",
+    },
+    { shipmentID: 2, arrivalDate: "June 3,2020 4:40PM EST", port: "Newark" },
+    { shipmentID: 3, arrivalDate: "June 3,2020 4:43PM EST", port: "Busan" },
+    { shipmentID: 4, arrivalDate: "June 4,2020 4:45PM EST", port: "Busan" },
+    { shipmentID: 5, arrivalDate: "June 4,2020 4:45PM EST", port: "Singapore" },
+    { shipmentID: 6, arrivalDate: "June 4,2020 4:45PM EST", port: "Hong Kong" },
+  ];
+  res.json(fakeData);
 });
 
 module.exports = router;
